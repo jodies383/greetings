7925e812c52e1eb36eaddbf4d7414b18d790d3a1
jodies383 <jodies383@gmail.com>
2021-05-07 14:18:46 +0200

    Error messages fixed

--- greet.js
+++ greet.js
@@ -12,19 +12,29 @@ var warnName = document.querySelector(".warningName")
 
 var warningLang = document.querySelector(".warningLang")
 
-var greetCounter = 1;
+var warnBoth = document.querySelector(".warningBoth")
+
+var greetCounter = 0;
 
 greetBtn.addEventListener('click', greetMe);
 greetBtn.addEventListener('click', updateCounter);
-greetBtn.addEventListener('click', noName)
+greetBtn.addEventListener('click', noName);
+
 
 resetBtn.addEventListener('click', function () {
   localStorage.removeItem('counted');
 });
 
-greetBtn.addEventListener('click', function () {
-  greetCounter++;
 
+
+greetBtn.addEventListener('click', function () {
+  var checkedRadioBtn = document.querySelector("input[name='languages']:checked");
+  var param1 = enterName.value
+
+
+  if (param1 = enterName.value && checkedRadioBtn) {
+    greetCounter++;
+  }
   localStorage['counted'] = greetCounter;
 });
 
@@ -36,29 +46,40 @@ if (localStorage['counted']) {
 function greetMe(param1) {
   var checkedRadioBtn = document.querySelector("input[name='languages']:checked").value;
   var param1 = enterName.value
+
   if (checkedRadioBtn === "English") {
+    warningLang.innerHTML = ("")
     ansGreet.innerHTML = ("Hello, " + param1);
   }
   else if (checkedRadioBtn === "Afrikaans") {
+    warningLang.innerHTML = ("")
     ansGreet.innerHTML = ("Goeie More, " + param1);
   }
   else if (checkedRadioBtn === "isiXhosa") {
+    warningLang.innerHTML = ("")
     ansGreet.innerHTML = ("Molo, " + param1);
   }
 
-
-  return ansGreet;
 }
 function noName(param1) {
   var checkedRadioBtn = document.querySelector("input[name='languages']:checked");
   var param1 = enterName.value
-  if (param1 === "") {
-    return warnName.innerHTML = ("Please enter your name");
-  } if (!checkedRadioBtn){
-    return warningLang.innerHTML = ("Please select a language");
-  } if (param1 === "" && !checkedRadioBtn){
-    return warningLang.innerHTML = ("Please enter your name and select a language")
+  if ((param1 === "" && !checkedRadioBtn)) {
+    warningLang.innerHTML = ("")
+    warnBoth.innerHTML = ("Please enter your name and select a language")
+  } else {
+    warnBoth.innerHTML = ("")
   }
+  if (param1 === "" && checkedRadioBtn) {
+    ansGreet.innerHTML = ("")
+    warnName.innerHTML = ("Please enter your name");
+  } else if (!checkedRadioBtn && param1) {
+    warningLang.innerHTML = ("Please select a language");
+  } else if (param1 === enterName.value) {
+    warnName.innerHTML = ("")
+
+  }
+
 }
 function updateCounter() {
 
