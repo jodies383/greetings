69d3a395b82afa52eb2e4430125518934dcb023f
jodies383 <jodies383@gmail.com>
2021-05-10 16:21:32 +0200

    Counter added

--- greet.js
+++ greet.js
@@ -14,28 +14,50 @@ var warningLang = document.querySelector(".warningLang")
 
 var warnBoth = document.querySelector(".warningBoth")
 
-var greetCounter = 0;
+// var greetCounter = 0;
 
 greetBtn.addEventListener('click', greetMe);
 greetBtn.addEventListener('click', updateCounter);
 greetBtn.addEventListener('click', noName);
-
+greetBtn.addEventListener('click', addNames);
 
 resetBtn.addEventListener('click', function () {
-  localStorage.removeItem('counted');
+  localStorage.removeItem('counted'),
+    localStorage.removeItem('namesList');
 });
 
 
+var existingNames = []
+
+if(localStorage['namesList']){
+   existingNames = JSON.parse(localStorage.getItem('namesList'));
+
+}
+
+var namesGreeted = existingNames || []
+
+
+function addNames() {
+
+
+    if(!namesGreeted.includes(enterName.value)){
+      namesGreeted.push(enterName.value)
+
+    }
+
+      localStorage.setItem('namesList', JSON.stringify(namesGreeted));
+
+      theCount.innerHTML = namesGreeted.length;
+    }
 
 greetBtn.addEventListener('click', function () {
   var checkedRadioBtn = document.querySelector("input[name='languages']:checked");
   var param1 = enterName.value
 
-
-  if (param1 = enterName.value && checkedRadioBtn) {
-    greetCounter++;
+  if (param1 === enterName.value && checkedRadioBtn) {
+    
   }
-  localStorage['counted'] = greetCounter;
+  
 });
 
 
@@ -83,6 +105,11 @@ function noName(param1) {
 }
 function updateCounter() {
 
-  return theCount.innerHTML = greetCounter;
+  return theCount.innerHTML = namesGreeted.length;
 
 }
+
+theCount.innerHTML = namesGreeted.length;
+
+
+
